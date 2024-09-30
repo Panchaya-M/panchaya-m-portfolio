8b6f5b858b5482702e3b6e98cfa0317e2811ee20
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2024-10-01 05:30:26 +0700

    create constants.js file

---
+++ src/constants/constants.js
@@ -0,0 +1,32 @@
+export const EXPERIENCES = [
+  {
+    company: "CODIUM Co., Ltd.",
+    start: "July 2022",
+    end: "Present",
+    job_position: "Frontend Developer",
+    responsibilities: [
+      "Created UI using Angular.",
+      "Designed and implemented UX/UI and responsive layouts.",
+      "Worked closely with backend developers to integrate APIs, connecting frontend components with backend services.",
+    ],
+    projects_intro: "I’ve had the chance to work on some exciting projects",
+    projects: [
+      {
+        name: "Credit information Recording System",
+        details:
+          "Developing a credit information request and document management recording system for Thailand's sole credit information provider.",
+      },
+      {
+        name: "e-Memo System",
+        details:
+          "Designed and implemented custom form and workflows tailored to client needs, enhancing internal communications and approval processes.",
+      },
+      {
+        name: "e-Budget System",
+        details:
+          "I had the opportunity to be 80% involved in building the UI for the newly developed prototype e-Budget System, which is a large-scale product for managing internal budgeting in organizations.",
+      },
+    ],
+    technologies_use: ["Angular", "TypeScript", "Bootstrap", "Gitlab", "Figma"],
+  },
+];
