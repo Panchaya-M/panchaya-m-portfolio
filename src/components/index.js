babff7d069bde2ceb0666c76b2089730ebf13058
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2024-10-01 07:29:07 +0700

    create contact section

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as Hero } from "./Hero";
 export { default as About } from "./About";
 export { default as Experience } from "./Experience";
 export { default as Skills } from "./Skills";
+export { default as Contact } from "./Contact";
 export { default as Carousel } from "./Carousel";
