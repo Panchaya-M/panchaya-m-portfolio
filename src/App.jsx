6e3cc44d98a9f60e0222e398dfc6888a4396e068
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2024-11-05 04:09:01 +0700

    add download cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { About, Contact, Experience, Hero, Navbar, Skills } from "./components";
+import cv from "./assets/files/cv.pdf"
 
 function App() {
   return (
@@ -8,6 +9,13 @@ function App() {
       <About />
       <Experience />
       <Skills />
+      <div className="flex justify-center items-center">
+        <a className="btn text-xs sm:text-base"
+          href={cv} download="cv-panchaya.pdf">
+          <i className="fa-solid fa-file-arrow-down pr-2 text-xl"></i>
+          Download CV
+        </a>
+      </div>
       <Contact />
     </div>
   );
